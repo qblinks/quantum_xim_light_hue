f864a63e797404238a37fbdddc87e2a4d71be922
Yi-Hua Liang <yihua1218@gmail.com>
2017-03-17 13:06:39 +0800

    Fixed internal server error when get_list_and_state result is fail.

--- discovery.js
+++ discovery.js
@@ -49,38 +49,40 @@ function discovery(options, callback) {
   const callback_options = JSON.parse(JSON.stringify(options));
   callback_options.list = [];
   callback_options.xim_content.lights = {};
+  callback_options.result = {};
   if (!options.xim_content.hue_access_token || !options.xim_content.bridgeid) {
     callback_options.result.err_no = 2;
     callback_options.result.err_msg = 'no token';
+    callback(callback_options);
   } else {
     callback_options.xim_content.lights = [];
     get_list_and_state(options.xim_content.hue_access_token,
     options.xim_content.bridgeid, (result) => {
-      callback_options.result = {};
       if (result === false || result.code === '404' || result.fault || result.code === '109') {
         callback_options.result.err_no = 1;
         callback_options.result.err_msg = 'fail';
         callback(callback_options);
+      } else {
+        callback_options.xim_content.lights = {};
+        Object.keys(result).forEach((key) => {
+          const light = {};
+          light.device_name = result[key].name;
+          light.device_id = key;
+          light.light_type = 'color';
+          light.infrared_support = false;
+          light.native_toggle_support = false;
+          light.light_status = {};
+          light.light_status.hue = parseInt((result[key].state.hue * 360) / 65534, 10);
+          light.light_status.saturation = parseInt((result[key].state.sat * 100) / 254, 10);
+          light.light_status.brightness = parseInt((result[key].state.bri * 100) / 254, 10);
+          light.light_status.onoff = result[key].state.on;
+          callback_options.xim_content.lights[key] = light;
+          callback_options.list.push(light);
+        });
+        callback_options.result.err_no = 0;
+        callback_options.result.err_msg = 'ok';
+        callback(callback_options);
       }
-      callback_options.xim_content.lights = {};
-      Object.keys(result).forEach((key) => {
-        const light = {};
-        light.device_name = result[key].name;
-        light.device_id = key;
-        light.light_type = 'color';
-        light.infrared_support = false;
-        light.native_toggle_support = false;
-        light.light_status = {};
-        light.light_status.hue = parseInt((result[key].state.hue * 360) / 65534, 10);
-        light.light_status.saturation = parseInt((result[key].state.sat * 100) / 254, 10);
-        light.light_status.brightness = parseInt((result[key].state.bri * 100) / 254, 10);
-        light.light_status.onoff = result[key].state.on;
-        callback_options.xim_content.lights[key] = light;
-        callback_options.list.push(light);
-      });
-      callback_options.result.err_no = 0;
-      callback_options.result.err_msg = 'ok';
-      callback(callback_options);
     });
   }
 }
