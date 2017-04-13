f5e6fd8dd7e5c71d0fcf1b3f934f9715767a7223
Yi-Hua Liang <yihua1218@gmail.com>
2017-04-13 21:41:28 +0800

    1. Modified error code.

--- authenticate.js
+++ authenticate.js
@@ -41,12 +41,14 @@ function get_hue_token(qblinks_token, set, tokenCb) {
  * @param {function} refCb callback of this function
  */
 function refresh_hue_token(qblinks_token, set, refCb) {
-  const options = { method: 'POST',
+  const options = {
+    method: 'POST',
     url: `${process.env.auth_url}/refresh_token/hue/${set}`,
-    headers:
-    {
-      authorization: `Bearer ${qblinks_token}` },
+    headers: {
+      authorization: `Bearer ${qblinks_token}`,
+    },
   };
+  // console.log(options);
   request(options, (error, response, body) => {
     if (error) throw new Error(error);
     const contact = JSON.parse(body);
@@ -62,14 +64,18 @@ function refresh_hue_token(qblinks_token, set, refCb) {
  */
 function get_bridge(hue_access_token, briCb) {
   let idValue;
-  const bridgesOptions = { method: 'GET',
+  const bridgesOptions = {
+    method: 'GET',
     url: 'https://api.meethue.com/v2/bridges',
-    headers:
-    {
-      authorization: `Bearer ${hue_access_token}` } };
+    headers: {
+      authorization: `Bearer ${hue_access_token}`,
+    },
+  };
+  console.log(bridgesOptions);
   request(bridgesOptions, (error, response, body) => {
     if (error) throw new Error(error);
     const contact = JSON.parse(body);
+    console.log(body);
     // if get bridgeid return this id
     if (contact[0]) {
       idValue = contact[0].id;
@@ -105,8 +111,8 @@ function authenticate(options, callback) {
           refresh_hue_token(callback_options.quantum_token,
             callback_options.xim_channel_set, (ref) => {
               callback_options.xim_content.hue_access_token = ref.access_token;
-              callback_options.result.err_no = 2;
-              callback_options.result.err_msg = 'please retry';
+              callback_options.result.err_no = 112;
+              callback_options.result.err_msg = 'Refresh Access Token';
               callback(callback_options);
             });
         } else {
