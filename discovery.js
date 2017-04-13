548e71c93ad84f217310812d5507eaeb5a6b49c4
Yi-Hua Liang <yihua1218@gmail.com>
2017-04-13 21:43:02 +0800

    Modified error code and error messages.

--- discovery.js
+++ discovery.js
@@ -32,7 +32,11 @@ function get_list_and_state(hue_access_token, bridgeid, get_state_callback) {
   };
   request(get_state_options, (error, response, body) => {
     if (error || body === 'Gateway Timeout') {
-      get_state_callback(false);
+      if (body === 'Gateway Timeout') {
+        get_state_callback(121);
+      } else {
+        get_state_callback(false);
+      }
     } else {
       const contact = JSON.parse(body);
       get_state_callback(contact);
@@ -49,18 +53,40 @@ function discovery(options, callback) {
   const callback_options = JSON.parse(JSON.stringify(options));
   callback_options.result = {};
   if (!options.xim_content.hue_access_token || !options.xim_content.bridgeid) {
-    callback_options.result.err_no = 2;
-    callback_options.result.err_msg = 'no token';
-    callback(callback_options);
+    if (!options.xim_content.hue_access_token) {
+      callback_options.result.err_no = 113;
+      callback_options.result.err_msg = 'No Access Token';
+      callback(callback_options);
+    } else if (!options.xim_content.bridgeid) {
+      callback_options.result.err_no = 120;
+      callback_options.result.err_msg = 'Invalid Bridge ID';
+      callback(callback_options);
+    } else {
+      callback_options.result.err_no = 900;
+      callback_options.result.err_msg = 'Unknown Error';
+      callback(callback_options);
+    }
   } else {
     callback_options.list = [];
     callback_options.groups = [];
     callback_options.xim_content.lights = [];
     get_list_and_state(options.xim_content.hue_access_token,
     options.xim_content.bridgeid, (result) => {
-      if (result === false || result.code === '404' || result.fault || result.code === '109') {
-        callback_options.result.err_no = 1;
-        callback_options.result.err_msg = 'fail';
+      if (result === false || result === 121 || result.code === '404' || result.fault || result.code === '109') {
+        if (result.fault && result.fault.detail && result.fault.detail.errorcode === 'keymanagement.service.access_token_expired') {
+          callback_options.result.err_no = 112;
+          callback_options.result.err_msg = 'Refresh Access Token';
+        } else if (result.fault && result.fault.detail && result.fault.detail.errorcode === 'keymanagement.service.invalid_access_token') {
+          callback_options.result.err_no = 110;
+          callback_options.result.err_msg = 'Invalid Access Token';
+        } else if (result === 121) {
+          callback_options.result.err_no = 121;
+          callback_options.result.err_msg = 'Bridge Timeout';
+        } else {
+          console.log(result);
+          callback_options.result.err_no = 900;
+          callback_options.result.err_msg = 'Unknow Error';
+        }
         callback(callback_options);
       } else {
         callback_options.xim_content.lights = {};
@@ -68,16 +94,40 @@ function discovery(options, callback) {
           const light = {};
           light.device_name = result.lights[key].name;
           light.device_id = `${key}`;
-          light.light_type = 'color';
+          if (result.lights[key].type === 'Dimmable light' ||
+              result.lights[key].type === 'Dimmable plug-in unit') {
+            light.light_type = 'dimmer';
+          } else if (result.lights[key].type === 'On/Off light') {
+            light.light_type = 'white';
+          } else if (result.lights[key].type === 'Color temperature light') {
+            light.light_type = 'color';
+          } else {
+            light.light_type = 'color';
+          }
           light.infrared_support = false;
           light.native_toggle_support = false;
           light.light_status = {};
-          light.light_status.hue = parseInt((result.lights[key].state.hue * 360) / 65534, 10);
-          light.light_status.saturation = parseInt((result.lights[key].state.sat * 100) / 254, 10);
-          light.light_status.brightness = parseInt((result.lights[key].state.bri * 100) / 254, 10);
+          if (light.light_type === 'color' && result.lights[key].type !== 'Color temperature light' && result.lights[key].type !== 'On/Off plug-in unit') {
+            light.light_status.hue = parseInt((result.lights[key].state.hue * 360) / 65534, 10);
+            light.light_status.saturation =
+              parseInt((result.lights[key].state.sat * 100) / 254, 10);
+
+            if (isNaN(light.light_status.hue)) {
+              console.log(result.lights[key]);
+            }
+          }
+
+          if (result.lights[key].type !== 'On/Off light') {
+            light.light_status.brightness =
+              parseInt((result.lights[key].state.bri * 100) / 254, 10);
+          }
+
           light.light_status.onoff = result.lights[key].state.on;
           callback_options.xim_content.lights[key] = light;
-          callback_options.list.push(light);
+
+          if (result.lights[key].type !== 'On/Off plug-in unit') {
+            callback_options.list.push(light);
+          }
         });
         if (typeof result.groups !== 'undefined') {
           Object.keys(result.groups).forEach((groupkey) => {
