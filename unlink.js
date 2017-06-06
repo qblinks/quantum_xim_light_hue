fac38a75c995507c12c72ae618ae1903cfd55ef4
Yi-Hua Liang <yihua1218@gmail.com>
2017-06-06 16:59:59 +0800

    1. Unlink return default result err_no = 0 and err_msg = 'ok’. 2. Added test script and event sample for unlink lambda local.

--- unlink.js
+++ unlink.js
@@ -11,6 +11,8 @@
 
 'use strict';
 
+const merge = require('merge');
+
  /**
   * Deactivate this channel
   *
@@ -20,7 +22,12 @@
   * @param {function} callback to be used by the XIM driver
   */
 function unlink(options, callback) {
-  callback(options);
+  const output = merge({}, options);
+  output.result = {
+    err_no: 0,
+    err_msg: 'ok',
+  };
+  callback(output);
 }
 
 /**
